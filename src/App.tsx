cd2cfad7607382d4a291fb3ea2933ef98ff8321d
RezaHeshmati <rezaheshmatidev@gmail.com>
2022-05-17 19:44:23 +0430

    (app): add search component  to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import useGetCountries from "./apiCalls/useGetCountries";
+import React from "react";
+import useGetCountries, {
+  ResponseType as countriesType,
+} from "./apiCalls/useGetCountries";
 import styles from "./App.module.css";
 import Card from "./components/card";
 import Header from "./components/header";
+import Search from "./components/search";
 
 const App = () => {
   const { loading, countries, error } = useGetCountries();
-  if (loading) return <div>loading</div>;
+  const [filteredCountries, setFilterdCountries] = React.useState<
+    countriesType | undefined
+  >(undefined);
+
+  React.useEffect(() => {
+    if (countries) setFilterdCountries([...countries]);
+  }, [countries]);
+
+  const handleOnSearch = (searchedCountry: string) => {
+    const filteredValue = countries?.filter((fc) =>
+      fc.name.toLowerCase().includes(searchedCountry)
+    );
+    if (filteredValue?.length) setFilterdCountries([...filteredValue]);
+    else setFilterdCountries(undefined);
+  };
+
+  if (loading) return <div>loading...</div>;
   if (error) return <div>opps....something went wrong!</div>;
   return (
     <div className={styles.App}>
       <Header />
+      <br />
+      <div id={styles.search_bar}>
+        <Search handleSearch={handleOnSearch} />
+      </div>
       <div id={styles.cards_container}>
-        {countries?.map((country) => (
-          <Card
-            name={country.name}
-            flag={country.flag}
-            capital={country.capital}
-            region={country.region}
-          />
-        ))}
+        {filteredCountries ? (
+          filteredCountries?.map(({ name, flag, capital, region }) => (
+            <Card name={name} flag={flag} capital={capital} region={region} />
+          ))
+        ) : (
+          <div>there is no country!</div>
+        )}
       </div>
     </div>
   );
