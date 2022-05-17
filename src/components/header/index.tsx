a4530d280ae234c7867058a5a0de281758e18861
RezaHeshmati <rezaheshmatidev@gmail.com>
2022-05-17 18:25:07 +0430

    (components): add a sample header component and its styles

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header id={styles.header_container}>
+      <h2>Countries Sample App</h2>
+    </header>
+  );
+};
+
+export default Header;
